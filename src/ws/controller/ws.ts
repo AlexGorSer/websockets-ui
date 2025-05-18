959f90d7b314eb8b97b0c22153d538fb1057476f
AlexGorSer <taskekaminari@gmail.com>
2025-05-18 16:07:45 +0200

    feat: create game

--- src/ws/controller/ws.ts
+++ src/ws/controller/ws.ts
@@ -10,14 +10,14 @@ import { randomUUID } from 'node:crypto';
 export const clients = new Map();
 const createdUsers: UserArrI[] = [];
 const availableRoomArr: UpdateRoomI[] = [];
-// const currentRoom: UpdateRoomI = [];
+const roomInGame: UpdateRoomI[] = [];
 
 export const wsController = async (ws: WebSocket, wss: WebSocket.Server) => {
   const clientId = randomUUID();
   clients.set(clientId, ws);
 
   ws.on('message', async (message) => {
-    // console.log('Received message:', message.toString());
+    console.log('Received message:', message.toString());
     const parseJSON: RequestI = await JSON.parse(message.toString());
 
     if (parseJSON.type === 'reg') {
@@ -25,12 +25,13 @@ export const wsController = async (ws: WebSocket, wss: WebSocket.Server) => {
       await UpdateRoom(wss, availableRoomArr);
     }
     if (parseJSON.type === 'create_room') {
-      await createRoom();
+      await createRoom(clientId);
       // console.log('create');
       await UpdateRoom(wss, availableRoomArr);
     }
     if (parseJSON.type === 'add_user_to_room') {
       await addUserToRoom(parseJSON.data, clientId);
+      await createGame(roomInGame, clients);
       // console.log('add');
       await UpdateRoom(wss, availableRoomArr);
     }
@@ -39,7 +40,7 @@ export const wsController = async (ws: WebSocket, wss: WebSocket.Server) => {
   ws.on('close', () => {
     console.log(`Dis ${clientId}`);
     clients.delete(clientId);
-    console.log(clients);
+    // console.log(clients);
   });
 };
 
@@ -54,7 +55,6 @@ const createPlayer = async (
 
   createdUsers.push({ name, password, id });
   console.log(`User ${name} create ${id}`);
-  console.log(createdUsers);
 
   const sendUser: RequestI = {
     type: 'reg',
@@ -70,14 +70,20 @@ const createPlayer = async (
   ws.send(JSON.stringify(sendUser));
 };
 
-const createRoom = async () => {
+const createRoom = async (clientId: string) => {
   const indexRoom = randomUUID();
+  const current = createdUsers.find((user) => user.id === clientId);
+
+  if (!current) {
+    return;
+  }
+
   const room: UpdateRoomI = {
     roomId: indexRoom,
     roomUsers: [
       {
-        name: createdUsers[0].name,
-        index: createdUsers[0].id,
+        name: current?.name,
+        index: current?.id,
       },
     ],
   };
@@ -89,7 +95,6 @@ const UpdateRoom = async (
   wss: WebSocket.Server,
   arr: UpdateRoomI[] | UpdateRoomI,
 ) => {
-  console.log(JSON.stringify(arr));
   const sendUser: RequestI = {
     type: 'update_room',
     data: JSON.stringify(arr),
@@ -109,10 +114,39 @@ const addUserToRoom = async (json: string, clientId: string) => {
     const user = room.roomUsers.find((user) => user.index === current.id);
 
     if (!user) {
-      console.log(current);
+      // console.log(current);
       const { name, id } = current;
       room.roomUsers.push({ name: name, index: id });
-      console.log(availableRoomArr);
+      // console.log(availableRoomArr);
+    }
+    if (roomInGame.length === 1) {
+      return;
+    } else {
+      roomInGame.push(room);
+      const index = availableRoomArr.indexOf(room);
+      availableRoomArr.slice(index, 0);
     }
   }
+};
+
+const createGame = async (
+  gameRoom: UpdateRoomI[],
+  clientsMap: Map<string | number, WebSocket>,
+) => {
+  gameRoom.map((elem) => {
+    if (elem.roomUsers.length !== 1) {
+      elem.roomUsers.forEach((room) => {
+        const ws = clientsMap.get(room.index);
+        const sendUser: RequestI = {
+          type: 'create_game',
+          data: JSON.stringify({
+            idGame: elem.roomId,
+            idPlayer: room.index,
+          }),
+          id: 0,
+        };
+        ws?.send(JSON.stringify(sendUser));
+      });
+    }
+  });
 };
